a19956e43362145c6341e95cfb8b096110b4f9dc
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2024-08-05 17:39:00 +0500

    application.properties env

--- src/main/java/com/time/demo/repository/UserImageRepository.java
+++ src/main/java/com/time/demo/repository/UserImageRepository.java
@@ -2,8 +2,10 @@ package com.time.demo.repository;
 
 import com.time.demo.entity.UserImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
 }
--- src/main/java/com/time/demo/repository/UserRepository.java
+++ src/main/java/com/time/demo/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.time.demo.repository;
 
 import com.time.demo.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
 }
